f7e79161a9fcaf069a5fb1440e33dd7ef00bc267
Francesco De Caro <francesco.decaro.dev@gmail.com>
2024-02-22 19:50:10 +0100

    debug.py update

--- debug.py
+++ debug.py
@@ -1,11 +1,8 @@
 import uuid
-import zlib
 
 from protopy import ProtoPY
-from protopy.datatypes.varint import Varint
 
 from protopy.packets.clientbountpackets import PlayerChatMessagePacket
-from protopy.utils import logger
 
 host = 'localhost'
 port = 25565
@@ -16,4 +13,4 @@ client.login('XSteve', uuid.UUID('6accdd58-b3fb-4c15-a87b-335b717627b3'))
 @client.listener()
 def _l(packet):
     if(isinstance(packet, PlayerChatMessagePacket)):
-        logger.info(packet.message)
+        print(packet.message)
