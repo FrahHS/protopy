6065b198a72b5475ee71901f0887c01f84b9bdc1
Francesco De Caro <francesco.decaro.dev@gmail.com>
2024-02-22 16:17:26 +0100

    packet reading method update

--- protopy/packets/clientbountpackets/clientboundpacket.py
+++ protopy/packets/clientbountpackets/clientboundpacket.py
@@ -7,10 +7,11 @@ class ClientBoundPacket(Packet, ABC):
     def __init__(self, raw_data: bytes, is_compressed: bool = False) -> None:
         super().__init__(is_compressed)
         self.raw_data = raw_data
+        self.response = {}
 
         self.packet_reader = PacketReader(self.is_compressed)
-        id, self.payload = self.packet_reader.get_packet_id_and_data(self.raw_data)
-        self.response = {}
+        self.payload = self.packet_reader.get_packet_id_and_data(self.raw_data)[1]
+
         self._read(self.payload)
 
     @abstractmethod
